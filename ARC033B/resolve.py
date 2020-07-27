a34be9814ea77e31943ce3f91a2ed30ea9083be0
Sho Taguchi <taguchi.sho@mail.canon>
2020-07-27 12:57:36 +0900

    あああ

--- ARC033B/resolve.py
+++ ARC033B/resolve.py
@@ -7,20 +7,13 @@ def resolve():
     As = [int(item) for item in input().split()]
     Bs = [int(item) for item in input().split()]
 
-    A_memo = [0 for _ in range(10**9+1)]
-    B_memo = [0 for _ in range(10**9+1)]
-
-    for item in As:
-        A_memo[item] = 1
-
-    for item in Bs:
-        B_memo[item] = 1
     
     A_and_B = []
     A_or_B = []
-    for i in range(10**5+1):
-        A_and_B.append(A_memo[i]&B_memo[i])
-        A_or_B.append(A_memo[i]|B_memo[i])
+    for item in As:
+        #二分探索でBsと同じの見つける
+        
+ 
 
     print(sum(A_and_B)/sum(A_or_B))
 
