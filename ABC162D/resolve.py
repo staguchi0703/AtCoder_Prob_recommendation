bf3bb5053f2d3c4ca27937cd9dbcb004bfd65ad7
sho Taguchi <s.taguchi0703@gmail.com>
2020-07-28 23:22:34 +0900

    ああ

--- ABC162D/resolve.py
+++ ABC162D/resolve.py
@@ -2,37 +2,20 @@ def resolve():
     '''
     code here
     '''
+    import collections
     N = int(input())
     S = input()
 
-    R_sum = [0 for _ in range(N)]
-    G_sum = [0 for _ in range(N)]
-    B_sum = [0 for _ in range(N)]
-
-    if S[0] == 'R':
-        R_sum[0] += 1
-    elif S[0] == 'G':
-        G_sum[0] += 1
-    else:
-        B_sum[0] += 1
-
-    for i in range(1, N):
-        if S[i] == 'R':
-            R_sum[i] += 1
-            R_sum[i] += R_sum[i-1]
-        elif S[i] == 'G':
-            G_sum[i] += 1
-            G_sum[i] += G_sum[i-1]
-        else:
-            B_sum[i] += 1
-            B_sum[i] += B_sum[i-1]
-    # 外す条件を調べないと
-    res = 0
-    for k in range(1,N-1):
-        if S[k] == 'R':
-            res += G_sum[k-1] 
-
-
+    cnt_dict = collections.Counter([item for item in S])
+    ng_cnt = 0
+    for i in range(N):
+        for j in range(i, N):
+            k = 2*j - i
+            if k < N:
+                if S[i] != S[j] and S[j] != S[k] and S[i] != S[k]:
+                    ng_cnt += 1
+    
+    print(cnt_dict['R']*cnt_dict['G']*cnt_dict['B'] - ng_cnt)
 
 
 if __name__ == "__main__":
