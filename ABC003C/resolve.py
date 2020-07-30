6e59f5b821b05b45f5d20971ea64fd228e32cf15
Sho Taguchi <taguchi.sho@mail.canon>
2020-07-30 12:52:48 +0900

    aa

--- ABC003C/resolve.py
+++ ABC003C/resolve.py
@@ -2,6 +2,17 @@ def resolve():
     '''
     code here
     '''
+    N, K = [int(item) for item in input().split()]
+    Rs = [int(item) for item in input().split()]
+
+    Rs.sort()
+    res = 0
+
+    for i in range(N-K, N):
+        res = (res + Rs[i])/2
+
+
+    print(res)
 
 
 if __name__ == "__main__":
