41a98c1c337a115ccfd4d5e42d4b98706ed46944
sho Taguchi <s.taguchi0703@gmail.com>
2020-07-31 23:08:40 +0900

    aa

--- ABC085D/test_pasted_from_page.py
+++ ABC085D/test_pasted_from_page.py
@@ -4,3 +4,56 @@ from resolve import resolve
 ####################################
 # 以下にプラグインの内容をペーストする
 #  
+import sys
+from io import StringIO
+import unittest
+
+
+class TestClass(unittest.TestCase):
+    def assertIO(self, input, output):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        out = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        print('------------')
+        print(out)
+        print('------------')
+        self.assertEqual(out, output)
+
+    def test_入力例_1(self):
+        input = """1 1
+3 5"""
+        output = """1"""
+        self.assertIO(input, output)
+
+#     def test_入力例_2(self):
+#         input = """2 10
+# 3 5
+# 2 6"""
+#         output = """2"""
+#         self.assertIO(input, output)
+
+#     def test_入力例_3(self):
+#         input = """4 1000000000
+# 1 1
+# 1 10000000
+# 1 30000000
+# 1 99999999"""
+#         output = """860000004"""
+#         self.assertIO(input, output)
+
+#     def test_入力例_4(self):
+#         input = """5 500
+# 35 44
+# 28 83
+# 46 62
+# 31 79
+# 40 43"""
+#         output = """9"""
+#         self.assertIO(input, output)
+
+
+if __name__ == "__main__":
+    unittest.main()
