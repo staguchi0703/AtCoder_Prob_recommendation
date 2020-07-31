00bbabe59538ce2f913a460af5816c97fc38dd8a
Sho Taguchi <taguchi.sho@mail.canon>
2020-07-31 12:59:48 +0900

    ss

--- ABC110C/resolve.py
+++ ABC110C/resolve.py
@@ -2,18 +2,36 @@ def resolve():
     '''
     code here
     '''
-    import collections
-    S = [item for item in input()]
-    T = [item for item in input()]
+    N, H = [int(item) for item in input().split()]
+    ab = [[int(item) for item in input().split()] for _ in range(N)]
 
-    S_cnt = collections.Counter(S)
-    T_cnt = collections.Counter(T)
+    a_max = max(ab, key=lambda x:x[0])
+    res = H // a_max
+    if H % a_max != 0:
+        res += 1
+    
+    temp_attack = res * a_max
+    b_delta = [b - a_max for a, b in ab]
 
+    throw_atk_sum = 0
+    throw_num = 0
 
-    if sorted(S_cnt.values()) == sorted(T_cnt.values()):
-            print('Yes')
+    for item in b_delta:
+        if b_delta > 0:
+            throw_atk_sum += item
+            throw_num += 1
+
+    if throw_atk_sum < temp_attack:
+        reduce_num = throw_atk_sum//a_max
+        print(res - reduce_num)
     else:
-        print('No')
+        res = 0
+        for a, b in ab:
+            H -= b
+            res += 1
+            if H <= 0:
+                break
+        print(res)
 
 
 if __name__ == "__main__":
